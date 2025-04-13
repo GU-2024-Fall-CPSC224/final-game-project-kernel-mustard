9cae5285b9d4d8f67cb56ea24a49b456579d2a7f
CSerafin27 <cserafin@zagmail.gonzaga.edu>
2025-04-13 12:25:47 -0700

    Add files via upload

--- src/test/java/edu/gonzaga/MainGameTest.java
+++ src/test/java/edu/gonzaga/MainGameTest.java
@@ -1,10 +1,7 @@
 package edu.gonzaga;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 
 public class MainGameTest {
     @Test
